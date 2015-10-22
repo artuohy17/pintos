4b4ab6fb36743ac19369f1fc74f2f09728118e88
Alec R Tuohy <alectuoh@timberlake.cse.buffalo.edu>
2015-10-21 22:32:24 -0400

    fixed max_priority

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -23,7 +23,7 @@ struct lock
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
     struct list_elem elem;      /* List element for priorty donation    */
-    struct max_priorty;         /* Max priorty donation                 */
+    int max_priorty;         /* Max priorty donation                 */
   };
 
 void lock_init (struct lock *);
